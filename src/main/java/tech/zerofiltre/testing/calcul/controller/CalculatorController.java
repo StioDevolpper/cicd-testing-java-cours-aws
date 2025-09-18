fe2c193946d0aec2f21b6320d4d8e0dcba9fdb26
khaled <mhadb.kh@gmail.com>
2025-09-18 16:00:36 +0200

    Clean code

--- src/main/java/tech/zerofiltre/testing/calcul/controller/CalculatorController.java
+++ src/main/java/tech/zerofiltre/testing/calcul/controller/CalculatorController.java
@@ -1,6 +1,4 @@
 package tech.zerofiltre.testing.calcul.controller;
-
-import javax.inject.Inject;
 import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
